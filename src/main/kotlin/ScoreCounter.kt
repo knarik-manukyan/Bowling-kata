46c0d336d29e2a0dd36848927df739142bffe5da
knarik-manukyan <knarmanukyan23@gmail.com>
2024-01-04 13:58:25 +0100

    handle strike with bonus points

--- src/main/kotlin/ScoreCounter.kt
+++ src/main/kotlin/ScoreCounter.kt
@@ -3,19 +3,29 @@ class ScoreCounter {
         return (throws.mapIndexed { id, singleThrow ->
             when(singleThrow) {
                 '/' -> handleSpare(throws, id)
-                'X' -> handleStrike()
-                else -> handleNumberScore(singleThrow)
+                'X' -> handleStrike(throws, id)
+                else -> handleNumberScore(throws, id)
             }
         }).sum()
     }
 
-    private fun handleNumberScore(singleThrow: Char) = singleThrow.digitToInt()
+    private fun handleNumberScore(throws: Array<Char>, id: Int): Int {
+        return when(val singleThrow = throws[id]) {
+            'X' -> 10
+            '/' -> 10 - handleNumberScore(throws, id - 1)
+            else -> singleThrow.digitToInt()
+        }
+    }
 
     private fun handleSpare(throws: Array<Char>, id: Int): Int {
-        val pointsForThisThrow = 10 - handleNumberScore(throws[id - 1])
-        val bonusPoints = handleNumberScore(throws[id + 1])
+        val pointsForThisThrow = handleNumberScore(throws, id)
+        val bonusPoints = handleNumberScore(throws, id + 1)
         return pointsForThisThrow + bonusPoints
     }
 
-    private fun handleStrike() = 10
+    private fun handleStrike(throws: Array<Char>, id: Int): Int {
+        val pointsForThisThrow = handleNumberScore(throws, id)
+        val bonusPoints = handleNumberScore(throws, id + 1) + handleNumberScore(throws, id + 2)
+        return pointsForThisThrow + bonusPoints
+    }
 }
--- src/test/kotlin/ScoreCounterTest.kt
+++ src/test/kotlin/ScoreCounterTest.kt
@@ -6,14 +6,14 @@ class ScoreCounterTest {
     fun `check simple case with no spare and strike`() {
         val sc = ScoreCounter()
         // modulo 6 is taken to have a valid game, where spare and strikes are not present
-        val game = (1..20).map { (it%6).digitToChar() }.toTypedArray()
+        val game = getGame(1)
         assertEquals(48, sc.countScore(game))
     }
 
     @Test
     fun `check spare with bonus`() {
         val sc = ScoreCounter()
-        val game = (1..20).map { (it%6).digitToChar() }.toTypedArray()
+        val game = getGame(1)
         // make second throw spare
         game[1] = '/'
         assertEquals(58, sc.countScore(game))
@@ -22,7 +22,7 @@ class ScoreCounterTest {
     @Test
     fun `check two spare`() {
         val sc = ScoreCounter()
-        val game = (1..20).map { (it%6).digitToChar() }.toTypedArray()
+        val game = getGame(1)
         // make second throw spare
         game[1] = '/'
         // make forth throw spare
@@ -31,12 +31,44 @@ class ScoreCounterTest {
     }
 
     @Test
-    fun `check strike without bonus`() {
+    fun `check strike with bonus`() {
         val sc = ScoreCounter()
         // modulo 6 is taken to have a valid game, where spare and strikes are not present
-        var game = (3..20).map { (it%6).digitToChar() }.toTypedArray()
+        var game = getGame(3)
         // make first throw strike
         game = arrayOf('X') + game
-        assertEquals(55, sc.countScore(game))
+        assertEquals(62, sc.countScore(game))
     }
+
+    @Test
+    fun `check two strike`() {
+        val sc = ScoreCounter()
+        // modulo 6 is taken to have a valid game, where spare and strikes are not present
+        var game = getGame(5)
+        // make first two throw strike
+        game = arrayOf('X', 'X') + game
+        assertEquals(78, sc.countScore(game))
+    }
+
+    @Test
+    fun `check three strike`() {
+        val sc = ScoreCounter()
+        // modulo 6 is taken to have a valid game, where spare and strikes are not present
+        var game = getGame(7)
+        // make first three throw strike
+        game = arrayOf('X', 'X', 'X') + game
+        assertEquals(97, sc.countScore(game))
+    }
+
+    @Test
+    fun `check strike and spare`() {
+        val sc = ScoreCounter()
+        // modulo 6 is taken to have a valid game, where spare and strikes are not present
+        var game = getGame(5)
+        // make first throw strike, and third one spare
+        game = arrayOf('X', '5', '/') + game
+        assertEquals(73, sc.countScore(game))
+    }
+
+    private fun getGame(startIndex: Int) = (startIndex..20).map { (it % 6).digitToChar() }.toTypedArray()
 }
